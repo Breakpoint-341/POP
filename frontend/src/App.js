a4ebbbf5f4d69f1de224e2b9aa8ff5b1eee9bb25
Gonzalo Daniel Aguilar <42863568+gonzalolater@users.noreply.github.com>
2023-11-27 23:52:51 -0300

    background of Home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-
+import Home from './components/Home'
 
 const App = () => {
   return (
-    <div className="body">
+    <div>
       <div>
-        <Navbar/>
+        <Navbar />
+        <Home />
         {/* Other components and content */}
       </div>
     </div>
