3ce92158575874768e8cdf23ec6468cdae5457e1
Gonzalo Daniel Aguilar <42863568+gonzalolater@users.noreply.github.com>
2023-11-28 00:46:38 -0300

    solve img responsive

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const MiComponente = () => {
   return (
     <div>
-        <img src="https://i.ibb.co/J2ztGPB/Constellations-hero.jpg" alt="Constellations-hero"  border="0" display='flex' width="100%" height="862vh" justifyContent='center'>
+        <img src="https://i.ibb.co/J2ztGPB/Constellations-hero.jpg" alt="Constellations-hero"  border="0" display='flex' width="100%" height="863vh" justifyContent='center'>
         </img>
     </div>
   );
